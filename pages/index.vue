6f853f78a96c4c6e3d1059b21ebec3660639d033
westlin <932420983@qq.com>
2019-09-04 20:03:28 +0800

    m完成首页轮播图和搜索框

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,218 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        cloud
-      </h1>
-      <h2 class="subtitle">
-        My spectacular Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <!-- 幻灯片 -->
+    <el-carousel :interval="5000" arrow="hover">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                background:url(${$axios.defaults.baseURL+item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active: index === currentOption}"
+            @click="handleOption(index)"
+          >
+            <i>{{item.name}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input
+            :placeholder="options[currentOption].placeholder"
+            v-model="searchValue"
+            @keyup.enter="handleSearch"
+          />
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </el-row>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      // 轮播图数据
+      banners: [
+        // {
+        //     url: "http://157.122.54.189:9095/assets/images/th01.jfif",
+        // },
+        // {
+        //     url: "http://157.122.54.189:9095/assets/images/th02.jfif",
+        // },
+        // {
+        //     url: "http://157.122.54.189:9095/assets/images/th03.jfif",
+        // },
+        // {
+        //     url: "http://157.122.54.189:9095/assets/images/th04.jfif",
+        // }
+      ],
+      options: [
+        // 搜索框tab选项
+        {
+          name: "攻略",
+          placeholder: "搜索城市",
+          pageUrl: "/post?city="
+        },
+        {
+          name: "酒店",
+          placeholder: "请输入城市搜索酒店",
+          pageUrl: "/hotel?city="
+        },
+        {
+          name: "机票",
+          placeholder: "请输入出发地",
+          pageUrl: "/air"
+        }
+      ],
+      searchValue: "", // 搜索框的值
+      currentOption: 0 // 当前选中的选项
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "scenics/banners"
+    }).then(res => {
+      const data = res.data.data;
+      this.banners = data;
+      console.log(res);
+    });
+  },
+  methods: {
+    handleOption(index) {
+      // 设置当前tab
+      this.currentOption = index;
+      // this.searchValue = '';
+      // 如果切换的机票tab，那么直接跳转到机票首页
+      const item = this.options[index];
+      if (item.name === "机票") {
+        this.$router.push(item.pageUrl);
+      }
+    },
+    handleSearch() {
+      const item = this.options[this.currentOption];
+      this.$router.push(item.pageUrl + this.searchValue);
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scoped lang="less">
 .container {
+  min-width: 1000px;
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  position: relative;
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  /deep/ .el-carousel__container {
+    height: 880px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
 
-.links {
-  padding-top: 15px;
+  .banner-content {
+    z-index: 2;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
+  }
 }
 </style>
