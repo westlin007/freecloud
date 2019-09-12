c2f10cc4c5dd7fe4d76d07081b1bac429b9b89f9
westlin <932420983@qq.com>
2019-09-12 11:21:03 +0800

    完成机票订单

---
+++ pages/air/order.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between">
+      <!-- 订单表单 -->
+      <OrderForm :data="infoData" />
+
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import OrderForm from "@/components/air/orderForm.vue";
+export default {
+  components: {
+    OrderForm
+  },
+  data() {
+    return {
+      // 机票信息
+      infoData: {
+        insurances: [] // 初始化保险数据
+      }
+    };
+  },
+  mounted(){
+    const {query} = this.$route;
+    this.$axios({
+        url:`airs/${query.id}`,
+        params:{
+            seat_xid: query.seat_xid
+        }
+    }).then(res=>{
+        this.infoData = res.data;
+    })
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 1000px;
+  margin: 20px auto;
+}
+.aside {
+  width: 350px;
+  height: fit-content;
+  border: 1px #ddd solid;
+}
+</style>
